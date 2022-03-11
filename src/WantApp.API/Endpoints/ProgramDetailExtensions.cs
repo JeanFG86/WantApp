84e6c2a1a01684b768132d50103426c1ea66cadc
jeanfg86 <jeanfg86@gmail.com>
2022-03-11 18:06:04 -0300

    Add Extension Method to Flunt Validation

---
+++ src/WantApp.API/Endpoints/ProgramDetailExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Flunt.Notifications;
+
+namespace WantApp.API.Endpoints;
+
+public static class ProgramDetailExtensions
+{
+    public static Dictionary<string, string[]> ConvertToProblemDetails(this IReadOnlyCollection<Notification> notifications)
+    {
+        return notifications.GroupBy(x => x.Key)
+                .ToDictionary(x => x.Key, x => x.Select(y => y.Message).ToArray());
+    }
+}
