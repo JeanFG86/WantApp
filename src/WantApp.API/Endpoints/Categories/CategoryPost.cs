75c3b16d7d79c8860df7a5df27e23a346aa321c1
jeanfg86 <jeanfg86@gmail.com>
2022-03-14 20:15:09 -0300

    refactor: make categoryPost to async

--- src/WantApp.API/Endpoints/Categories/CategoryPost.cs
+++ src/WantApp.API/Endpoints/Categories/CategoryPost.cs
@@ -9,10 +9,10 @@ public class CategoryPost
 {
     public static string Template => "/categories";
     public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
-    public static Delegate Handle => Action;
+    public static Delegate Handle => ActionAsync;
 
     [Authorize(Policy = "EmployeePolicy")]
-    public static IResult Action(CategoryRequest categoryRequest, HttpContext http, ApplicationDbContext context)
+    public static async Task<IResult> ActionAsync(CategoryRequest categoryRequest, HttpContext http, ApplicationDbContext context)
     {
         var userId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
         var category = new Category(categoryRequest.Name, "Test", "Test");
@@ -21,8 +21,8 @@ public class CategoryPost
         if (!category.IsValid)
             return Results.ValidationProblem(category.Notifications.ConvertToProblemDetails());
 
-        context.Categories.Add(category);
-        context.SaveChanges();
+        await context.Categories.AddAsync(category);
+        await context.SaveChangesAsync();
 
         return Results.Created($"/categories/{category.Id}", category.Id);
     }
