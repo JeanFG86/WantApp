6ecc0d8b7177fba4e7695003eb59cd98cdd30710
jeanfg86 <jeanfg86@gmail.com>
2022-03-14 16:19:15 -0300

    Add createdBy on EmployeePost

--- src/WantApp.API/Endpoints/Employees/EmployeePost.cs
+++ src/WantApp.API/Endpoints/Employees/EmployeePost.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
 
 namespace WantApp.API.Endpoints.Employees;
@@ -9,25 +10,28 @@ public class EmployeePost
     public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(EmployeeRequest employeeRequest, UserManager<IdentityUser> userManager)
+    [Authorize(Policy = "EmployeePolicy")]
+    public static IResult Action(EmployeeRequest employeeRequest, HttpContext http, UserManager<IdentityUser> userManager)
     {
-        var user = new IdentityUser { UserName =  employeeRequest.email, Email = employeeRequest.email };
-        var result = userManager.CreateAsync(user, employeeRequest.password).Result;
+        var userId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+        var newUser = new IdentityUser { UserName =  employeeRequest.email, Email = employeeRequest.email };
+        var result = userManager.CreateAsync(newUser, employeeRequest.password).Result;
 
         var userClaims = new List<Claim>
         {
             new Claim("EmployeeCode", employeeRequest.employeeCode),
-            new Claim("Name", employeeRequest.name)
+            new Claim("Name", employeeRequest.name),
+            new Claim("CreatedBy", userId),
         };
 
         if (!result.Succeeded)
             return Results.BadRequest(result.Errors.ConvertToProblemDetails());
 
-        var calimResult = userManager.AddClaimsAsync(user, userClaims).Result;
+        var calimResult = userManager.AddClaimsAsync(newUser, userClaims).Result;
 
         if (!calimResult.Succeeded)
             return Results.BadRequest(result.Errors.First());
 
-        return Results.Created($"/employees/{user.Id}", user.Id);
+        return Results.Created($"/employees/{newUser.Id}", newUser.Id);
     }
 }
