187c4331c99ab5d452411f8f0fda57b83b9cffb9
jeanfg86 <jeanfg86@gmail.com>
2022-03-16 17:35:52 -0300

    add clientGET

---
+++ src/WantApp.API/Endpoints/Client/ClientGet.cs
@@ -0,0 +1,25 @@
+﻿using WantApp.API.Domain.Users;
+
+namespace WantApp.API.Endpoints.Client;
+
+public class ClientGet
+{
+    public static string Template => "/clients";
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => ActionAsync;
+
+    [AllowAnonymous]
+    public static async Task<IResult> ActionAsync(HttpContext http)
+    {
+        var user = http.User;
+        var result = new
+        {
+            Id = user.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value,
+            Nome = user.Claims.First(c => c.Type == "Name").Value,
+            Cpf = user.Claims.First(c => c.Type == "Cpf").Value,
+        };
+
+        return Results.Ok(result);
+        
+    }
+}
--- src/WantApp.API/Program.cs
+++ src/WantApp.API/Program.cs
@@ -81,6 +81,7 @@ app.MapMethods(ProductPost.Template, ProductPost.Methods, ProductPost.Handle);
 app.MapMethods(ProductGetAll.Template, ProductGetAll.Methods, ProductGetAll.Handle);
 app.MapMethods(ProductGetShowCase.Template, ProductGetShowCase.Methods, ProductGetShowCase.Handle);
 app.MapMethods(ClientPost.Template, ClientPost.Methods, ClientPost.Handle);
+app.MapMethods(ClientGet.Template, ClientGet.Methods, ClientGet.Handle);
 
 app.UseExceptionHandler("/error");
 app.Map("/error", (HttpContext http) =>
